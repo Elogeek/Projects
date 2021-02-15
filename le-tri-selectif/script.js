68e7532807af8e40c4e935d8387346db64f57c2f
Elogeek <elodiechristin@gmail.com>
2021-02-15 15:48:27 +0100

    start and finish design site, start game area

--- le-tri-selectif/script.js
+++ le-tri-selectif/script.js
@@ -1,8 +1,23 @@
 //start the game
 
+let title = document.getElementById("title");
+let rules = document.getElementById("rules");
+let begin = document.getElementById("begin");
+let logo = document.getElementById("logo");
+let poubelle = document.getElementById("poubelle");
+let dechets = document.getElementById("dechets");
+
 function game(){
+    title.style.display = "none";
+    rules.style.display = "none";
+    begin.style.display = "none";
+    logo.style.display = "none";
+    poubelle.style.display = "flex";
+    dechets.style.display ="flex";
+    game.reload();
     // requête jSON
     //ouverture poubelle pour animer un peu le jeu?!
 }
 
+begin.addEventListener("click", game);
 
