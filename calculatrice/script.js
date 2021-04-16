137205522bfae17753d84f156690f6ec75659074
elogeek <elodiechristin@gmail.com>
2021-04-16 19:08:09 +0200

    go

--- calculatrice/script.js
+++ calculatrice/script.js
@@ -20,84 +20,20 @@ document.getElementById("chiffre5").addEventListener("click", function () {
     result.innerText = result.innerHTML + 5;
 });
 
-document.getElementById("chiffre6").addEventListener("click", function () {
-    result.innerText = result.innerHTML + 6;
-});
+document.querySelector("#reset").addEventListener("click", e => result.innerText = " ");
+document.querySelector("#equal").addEventListener("click", e =>  result.innerHTML = eval(result.innerHTML));
+document.querySelector("#delete").addEventListener("click", e =>
+    result.innerText = result.innerHTML.slice(0, result.innerHTML.length - 1)
+);
 
-document.getElementById("chiffre7").addEventListener("click", function () {
-    result.innerText = result.innerHTML + 7;
-});
-
-document.getElementById("chiffre8").addEventListener("click", function () {
-    result.innerText = result.innerHTML + 8;
-});
-
-document.getElementById("chiffre9").addEventListener("click", function () {
-    result.innerText = result.innerHTML + 9;
-});
-
-document.getElementById("chiffre0").addEventListener("click", function () {
-    result.innerText = result.innerHTML + 0;
-});
-
-document.getElementById("plus").addEventListener("click", function () {
-    result.innerText = result.innerHTML + "+";
-});
-
-document.getElementById("moins").addEventListener("click", function () {
-    result.innerText = result.innerHTML + "-";
-});
-
-document.getElementById("diviser").addEventListener("click", function () {
-   result.innerText = result.innerHTML + "/";
-});
-
-document.getElementById("fois").addEventListener("click", function () {
-    result.innerText = result.innerHTML + "*";
-});
-
-document.getElementById("parenthese").addEventListener("click", function () {
-    result.innerText = result.innerHTML + "(";
-});
-
-document.getElementById("parenthese2").addEventListener("click", function () {
-   result.innerText = result.innerHTML + ")";
-});
-
-document.getElementById("modulo").addEventListener("click", function () {
-    result.innerText = result.innerHTML + "%";
-});
-
-document.getElementById("virgule").addEventListener("click", function () {
-    result.innerText = result.innerHTML + ".";
-});
-
-document.getElementById("reset").addEventListener("click", function () {
-    result.innerText = " ";
-});
-
-document.getElementById("delete").addEventListener("click", function () {
-   result.innerText = result.innerHTML.slice(0, result.innerHTML.length - 1);
-});
-
-document.getElementById("egal").addEventListener("click", function () {
-    result.innerHTML = eval(result.innerHTML);
-});
-
-//dans le presse papier, l'événement copy
-document.getElementById("copy").addEventListener("click", function () {
-    let range = document.createRange();// je crée l'objet range (= segment qui peut contenir des noeuds et des parties de noeuds)
-    range.selectNode(result); // Sélection du texte
+// Dans le presse papier.
+document.querySelector("#copy").addEventListener("click",  e => {
+    // Range object creation ( contains nodes portions )
+    const range = document.createRange();
+    range.selectNode(result); // Result text selection
     window.getSelection().addRange(range);
-    try{
-    // l'instruction try catch permet de regrouper des instrictions à éxécuter et définit une réponse
-    // si l'une de ses instructions provoque une exception
-        if(document.execCommand('copy')){
-            alert("Bravo, la copie est réussi !");
-        }
-    window.getSelection().removeAllRanges();
-    }catch (error){
-        alert("Erreur, la copie n'a pas pu ce faire !");
+    if(document.execCommand('copy')){
+        alert("Copié!");
     }
 });
 
